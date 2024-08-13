b7931c7ea385534d575813e8784620dad70f1259
pkj <pkj2003456@gmail.com>
2024-08-13 02:01:33 +0000

    setup the dabase and set the new route for db,set up the log

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template, url_for, request, redirect, make_response, jsonify, session
+from flask import Flask, url_for, request, redirect, make_response, jsonify, session,abort
 from utils import call_txt2img_api, set_checkpoint, refresh, get_current_model, encode_file_to_base64, call_img2img_api, get_model_list
 import json
 from werkzeug.utils import secure_filename
@@ -7,10 +7,16 @@ import secrets
 from flask_cors import CORS
 import logging
 from logging.handlers import RotatingFileHandler
+from db import db  
+from models import User, Image 
+
+
 
 app = Flask(__name__)
 CORS(app)
-
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///D:\\stable_diffusion_database\\test1.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
 app.config['SESSION_TYPE'] = 'filesystem'
 app.root_path = os.path.dirname(os.path.abspath(__file__))
 app.config['UPLOAD_FOLDER'] = 'static/img/face'
@@ -26,11 +32,11 @@ logger = logging.getLogger('flask_app')
 logger.setLevel(logging.INFO)
 
 # File handler for info and above
-file_handler = RotatingFileHandler(log_file_path, maxBytes=5*1024*1024, backupCount=5)
+file_handler = RotatingFileHandler(log_file_path, maxBytes=1*1024*1024, backupCount=5)
 file_handler.setLevel(logging.INFO)
 
 # File handler for error and above
-error_file_handler = RotatingFileHandler(error_log_file_path, maxBytes=5*1024*1024, backupCount=5)
+error_file_handler = RotatingFileHandler(error_log_file_path, maxBytes=1*1024*1024, backupCount=5)
 error_file_handler.setLevel(logging.ERROR)
 
 # Formatter for all handlers
@@ -55,6 +61,29 @@ def method_not_allowed(e):
     response = jsonify({'error': 'Method Not Allowed'})
     response.status_code = 405
     return response
+
+@app.route('/add_user')
+def add_user():
+    new_user = User(username='test_user', email='test@example.com')
+    db.session.add(new_user)
+    db.session.commit()
+    return 'User added!'
+
+@app.route('/get_users')
+def get_users():
+    users = User.query.all()
+    return jsonify([user.username for user in users])
+
+@app.route('/users/<int:user_id>', methods=['GET'])
+def delete_user(user_id):
+    user = User.query.get(user_id)
+    if user is None:
+        abort(404, description="User not found")
+    
+    db.session.delete(user)
+    db.session.commit()
+    
+    return jsonify({'message': 'User deleted successfully'}), 200
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
@@ -127,6 +156,10 @@ def index():
         logger.info(f"Data after removing reactor_img: {data}")
         text2img_data = call_txt2img_api(control_pose, reactor_img, **data)
         # logger.info(f"Text2Img data: {text2img_data}")
+        with open('testapi/text2img_data.json','w') as f:
+            json.dump(text2img_data,f,indent=4)
+            logger.info("already gen to text2img_data.json")
+            
         return jsonify(text2img_data)
     else:
         logger.info("GET request to /txt2img")
@@ -153,6 +186,10 @@ def img2img():
         logger.error(f"Error in img2img: {e}")
         return jsonify({'error': 'Internal Server Error'}), 500
 
+with app.app_context():
+    db.create_all()
+    
 if __name__ == '__main__':
+
     from waitress import serve
     serve(app, host='0.0.0.0', port=8080, channel_timeout=600,channel_backlog=100)
